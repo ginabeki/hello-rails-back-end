df963f0a300605747229b93f2be911fb706c28d6
ginabeki <ginabeki3@gmail.com>
2023-04-15 16:00:00 +0300

    Add configuration to prevent cors error

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,8 @@
 class MessagesController < ApplicationController
   def random_greeting
+    response.headers['Access-Control-Allow-Origin'] = 'http://localhost:3001'
+    response.headers['Access-Control-Allow-Methods'] = 'GET'
+    response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
     message = Message.order('RANDOM()').first
     render json: { greeting: message.greeting }
   end
